9522f2a39f16d12aacaa6a18ed9d52962c51ef31
secovate200 <secovate200@gmail.com>
2026-01-02 21:22:32 +0900

    2026-01-02 blog layout and create navigationbar

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".nav");
+const searchIcon = document.querySelector("#searchIcon");
+const navOpenBtn = document.querySelector(".navOpenBtn");
+const navCloseBtn = document.querySelector(".navCloseBtn");
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("bx-search", "bx-x");
+  }
+  searchIcon.classList.replace("bx-x", "bx-search");
+});
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("bx-x", "bx-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
